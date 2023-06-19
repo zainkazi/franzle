a03997c289e927d332e5bb2b5bf0d67f96a69120
Zain Kazi <zainkazi27@gmail.com>
2023-06-19 03:02:38 -0700

    added dark theme

--- src/theme.ts
+++ src/theme.ts
@@ -8,6 +8,7 @@ import { extendTheme } from "@chakra-ui/react";
 const theme = extendTheme({
   config: {
     initialColorMode: "dark",
+    useSystemColorMode: false,
   },
   colors: {
     brand: {
