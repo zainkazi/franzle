eba7e69b3402163643e479ece3f2a1e05980f123
Zain Kazi <zainkazi27@gmail.com>
2023-06-19 08:43:57 -0700

    minor design changes

--- src/components/HeroDetails.tsx
+++ src/components/HeroDetails.tsx
@@ -18,11 +18,11 @@ const HeroDetails = () => {
         as={Box}
         textAlign={"center"}
         spacing={{ base: 8, md: 14 }}
-        py={{ base: 20, md: 36 }}
+        py={{ base: 32, md: 36 }}
       >
         <Heading
           fontWeight={600}
-          fontSize={{ base: "2xl", sm: "4xl", md: "6xl" }}
+          fontSize={{ base: "4xl", sm: "4xl", md: "6xl" }}
           lineHeight={"110%"}
         >
           Propel your business
